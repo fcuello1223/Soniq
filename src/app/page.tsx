60d33df4b1efd39d297cb3ea00ae279c776da3e6
Francisco Cuello <fcuel001@fiu.edu>
2026-08-04 17:43:50 -0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function Home() {
+  return (
+    <Button size="xs" onClick={() => toast.success("Buenos Diaz!")}>
+      Click Here
+    </Button>
+  );
+}
