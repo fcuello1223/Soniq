69beb7bd14f502c9b92f6f6a62521ed2efaddf2f
Francisco Cuello <fcuel001@fiu.edu>
2026-08-06 20:54:54 -0400

    Completed Clerk Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="xs" onClick={() => toast.success("Buenos Diaz!")}>
-      Click Here
-    </Button>
+    <div className="flex flex-col items-center justify-center gap-4 bg-background min-h-screen">
+      <h1 className="text-2xl font-semibold">Welcome to Soniq!</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
   );
 }
